ff4dc1ef204c70b95112ceefadd95d6b2f99468b
Rifat Cholakov <rifatcholakov@Gmail.com>
2020-09-11 16:48:33 +0300

    updating out app with a shop page, collection preview component and collection item component

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,249 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https://i.imgur.com/LGGXpNK.jpg',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.imgur.com/Jkf1Cda.jpg',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.imgur.com/pN8Bf39.jpg',
+                price: 35
+            },
+            {
+                id: 4,
+                name: 'Grey Brim',
+                imageUrl: 'https://i.imgur.com/vWwommr.jpg',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Green Beanie',
+                imageUrl: 'https://i.imgur.com/rbA3fqn.jpg',
+                price: 18
+            },
+            {
+                id: 6,
+                name: 'Palm Tree Cap',
+                imageUrl: 'https://i.imgur.com/OkMqPNX.jpg',
+                price: 14
+            },
+            {
+                id: 7,
+                name: 'Red Beanie',
+                imageUrl: 'https://i.imgur.com/QZ4f1AA.jpg',
+                price: 18
+            },
+            {
+                id: 8,
+                name: 'Wolf Cap',
+                imageUrl: 'https://i.imgur.com/zzzirRX.jpg',
+                price: 14
+            },
+            {
+                id: 9,
+                name: 'Blue Snapback',
+                imageUrl: 'https://i.imgur.com/CUPsR6a.jpg',
+                price: 16
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Sneakers',
+        routeName: 'sneakers',
+        items: [
+            {
+                id: 10,
+                name: 'Adidas NMD',
+                imageUrl: 'https://i.imgur.com/ERI3muL.jpg',
+                price: 220
+            },
+            {
+                id: 11,
+                name: 'Adidas Yeezy',
+                imageUrl: 'https://i.imgur.com/c4pMwhO.jpg',
+                price: 280
+            },
+            {
+                id: 12,
+                name: 'Black Converse',
+                imageUrl: 'https://i.imgur.com/CCSrvhR.jpg',
+                price: 110
+            },
+            {
+                id: 13,
+                name: 'Nike White AirForce',
+                imageUrl: 'https://i.imgur.com/TXHaG9C.jpg',
+                price: 160
+            },
+            {
+                id: 14,
+                name: 'Nike Red High Tops',
+                imageUrl: 'https://i.imgur.com/o39vTBR.jpg',
+                price: 160
+            },
+            {
+                id: 15,
+                name: 'Nike Brown High Tops',
+                imageUrl: 'https://i.imgur.com/mqcMegY.jpg',
+                price: 160
+            },
+            {
+                id: 16,
+                name: 'Air Jordan Limited',
+                imageUrl: 'https://i.imgur.com/95vRm1w.jpg',
+                price: 190
+            },
+            {
+                id: 17,
+                name: 'Timberlands',
+                imageUrl: 'https://i.imgur.com/hLEKxHx.jpg',
+                price: 200
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Jackets',
+        routeName: 'jackets',
+        items: [
+            {
+                id: 18,
+                name: 'Black Jean Shearling',
+                imageUrl: 'https://i.imgur.com/rXiEV0W.jpg',
+                price: 125
+            },
+            {
+                id: 19,
+                name: 'Blue Jean Jacket',
+                imageUrl: 'https://i.imgur.com/zGLiqfN.jpg',
+                price: 90
+            },
+            {
+                id: 20,
+                name: 'Grey Jean Jacket',
+                imageUrl: 'https://i.imgur.com/wudwDtW.jpg',
+                price: 90
+            },
+            {
+                id: 21,
+                name: 'Brown Shearling',
+                imageUrl: 'https://i.imgur.com/23HtFzD.jpg',
+                price: 165
+            },
+            {
+                id: 22,
+                name: 'Tan Trench',
+                imageUrl: 'https://i.imgur.com/6NOlLFJ.jpg',
+                price: 185
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Womens',
+        routeName: 'womens',
+        items: [
+            {
+                id: 23,
+                name: 'Blue Tanktop',
+                imageUrl: 'https://i.imgur.com/f5T9FEN.jpg',
+                price: 25
+            },
+            {
+                id: 24,
+                name: 'Floral Blouse',
+                imageUrl: 'https://i.imgur.com/32CteHB.jpg',
+                price: 20
+            },
+            {
+                id: 25,
+                name: 'Floral Dress',
+                imageUrl: 'https://i.imgur.com/EGupLL4.jpg',
+                price: 80
+            },
+            {
+                id: 26,
+                name: 'Red Dots Dress',
+                imageUrl: 'https://i.imgur.com/738m8Ub.jpg',
+                price: 80
+            },
+            {
+                id: 27,
+                name: 'Striped Sweater',
+                imageUrl: 'https://i.imgur.com/4VRG6iF.jpg',
+                price: 45
+            },
+            {
+                id: 28,
+                name: 'Yellow Track Suit',
+                imageUrl: 'https://i.imgur.com/ahZsKdb.jpg',
+                price: 135
+            },
+            {
+                id: 29,
+                name: 'White Blouse',
+                imageUrl: 'https://i.imgur.com/6WHI3eq.jpg',
+                price: 20
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'Mens',
+        routeName: 'mens',
+        items: [
+            {
+                id: 30,
+                name: 'Camo Down Vest',
+                imageUrl: 'https://i.imgur.com/w6EHILm.jpg',
+                price: 325
+            },
+            {
+                id: 31,
+                name: 'Floral T-shirt',
+                imageUrl: 'https://i.imgur.com/mWvuPuZ.jpg',
+                price: 20
+            },
+            {
+                id: 32,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.imgur.com/bvCtRkf.jpg',
+                price: 25
+            },
+            {
+                id: 33,
+                name: 'Pink T-shirt',
+                imageUrl: 'https://i.imgur.com/h8qw60M.jpg',
+                price: 25
+            },
+            {
+                id: 34,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.imgur.com/BlNKetq.jpg',
+                price: 40
+            },
+            {
+                id: 35,
+                name: 'Burgundy T-shirt',
+                imageUrl: 'https://i.imgur.com/2hrKotq.jpg',
+                price: 25
+            }
+        ]
+    }
+];
+
+export default SHOP_DATA;
