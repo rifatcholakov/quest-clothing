f7b4fb3e64763112ae092bfb45446467f6efc12d
Rifat Cholakov <rifatcholakov@Gmail.com>
2020-09-21 19:07:58 +0300

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_RzctdwZ0Uj1Kwnfa4RrWZwY3';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name="Quest Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image="https://i.imgur.com/mbMEW2N.png"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
